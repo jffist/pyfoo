8048b5b3cb0b732920f87a2cea88db2357f99d82
Taras Svirskyi <tsvirskyi@gocardless.com>
2019-07-25 18:29:46 +0100

    added doc string to scale function for illustration

--- pyfoo/mathfun.py
+++ pyfoo/mathfun.py
@@ -1,4 +1,11 @@
 import numpy as np
 
 def scale(x):
+    """ Centers the vector by subtracting it's mean and scales by the standart error
+    :param x: numeric vector
+    :type x: numpy.array or list of numbers
+
+    :returns: scaled vector of the same length with zero mean and deviation equal to 1
+    :rtype: numpy.array
+    """
     return (x - np.mean(x)) / np.std(x)
--- tests/test_mathfun.py
+++ tests/test_mathfun.py
@@ -2,4 +2,4 @@ import numpy.testing as npt
 from pyfoo.mathfun import scale
 
 def test_scale():
-    npt.assert_allclose([-1.22474487,  0.,  1.22474487], scale([-1, 0, 1]))
+    npt.assert_allclose([-1,  1], scale([-2, 0]))
