257313afb683ae5360208310641ef2a28229d540
Taras Svirskyi <tsvirskyi@gocardless.com>
2019-07-25 20:51:02 +0100

    removed obsolete main from foo module

--- pyfoo/foo.py
+++ pyfoo/foo.py
@@ -1,12 +1,3 @@
-import sys
-
 def bar(x):
     print(f"calling foo.bar with x={x}")
     return x+2
-
-def main():
-    if len(sys.argv)!=2:
-        print(f"Usage: {sys.argv[0]} N\nN - integer")
-    else:
-        res = bar(int(sys.argv[1]))
-        print('result:', res)
